e9dfff2e27faf6e346c3a98c4f7b3bb390863c1a
John Didion <github@didion.net>
2015-08-04 11:23:29 -0400

    don't write scientific format

--- R/ShapeIt.R
+++ R/ShapeIt.R
@@ -1,20 +1,26 @@
-bim.to.map <- function(bim, outfile) {
+bim.to.map <- function(bim, outfile, chrms=NULL) {
     if (is.character(bim)) {
         bim <- read.table(bim, sep="\t", header=F, stringsAsFactors=F)
-        for (chrm in unique(bim[,1])) {
-            b <- bim[bim[,1]==chrm,]
-            ppos <- b[,4]
-            gpos <- b[,3]
-            
-            pdiff <- diff(ppos) / 1000000
-            gdiff <- diff(gpos)
-            rrate <- gdiff / pdiff
-            # the rrate vector is one shoreter than p/gpos vectors, so we just
-            # duplicated the rrate of the first marker
-            rrate <- c(rrate[1], rrate)
-            
-            write.table(data.frame(pposition=ppos, rrate=rrate, gposition=gpos),
-                paste0(outfile, chrm, '.txt'), sep=" ", row.names=F, col.names=T, quote=F)
-        })
     }
+    if (is.null(chrms)) {
+        chrms <- unique(bim[,1])
+    }
+    scipen <- options(scipen)
+    options(scipen=10)
+    for (chrm in chrms) {
+        b <- bim[bim[,1]==chrm,]
+        ppos <- b[,4]
+        gpos <- b[,3]
+        
+        pdiff <- diff(ppos) / 1000000
+        gdiff <- diff(gpos)
+        rrate <- gdiff / pdiff
+        # the rrate vector is one shoreter than p/gpos vectors, so we just
+        # duplicated the rrate of the first marker
+        rrate <- c(rrate[1], rrate)
+        
+        write.table(data.frame(pposition=ppos, rrate=rrate, gposition=gpos),
+            paste0(outfile, chrm, '.txt'), sep=" ", row.names=F, col.names=T, quote=F)
+    }
+    options(scipen=scipen)
 }
